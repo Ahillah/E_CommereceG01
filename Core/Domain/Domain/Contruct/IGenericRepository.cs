ed345046c5488fb55e3c904cd80d347ec3862f35
أهلة <@>
2025-04-21 20:24:16 +0200

    V1

---
+++ Core/Domain/Domain/Contruct/IGenericRepository.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Contruct
+{
+    public interface IGenericRepository<TEntity,TKey> where TEntity :ModelBase<TKey>
+    {
+       Task< IEnumerable<TEntity> >GetAllAsync();
+
+        Task<TEntity> GetByIdAsync(TKey id);
+
+        void Add(TEntity entity);
+
+        void Update(TEntity entity);
+
+        void Delete(TEntity entity);
+    }
+}
---
+++ Infrastructure/Persistance/Repositories/GenericRepository.cs
@@ -0,0 +1,31 @@
+﻿using Domain.Contruct;
+using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Persistance.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistance.Repositories
+{
+    public class GenericRepository<TEntity, Tkey>(StoreDBContext context) : IGenericRepository<TEntity, Tkey>
+        where TEntity : ModelBase<Tkey>
+    {
+        public async Task<IEnumerable<TEntity>> GetAllAsync()
+       => await context.Set<TEntity>().ToListAsync();
+
+        public async Task<TEntity> GetByIdAsync(Tkey id)
+        => await context.Set<TEntity>().FindAsync(id);
+        public void Add(TEntity entity)
+       => context.Set<TEntity>().Add(entity);
+        public void Delete(TEntity entity)
+       => context?.Set<TEntity>().Remove(entity);
+
+    
+
+        public void Update(TEntity entity)
+        => context.Set<TEntity>().Update(entity);
+    }
+}
